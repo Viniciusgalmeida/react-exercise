d82f417de164f9bf3f919a18d1c283e2338da125
Viniciusgalmeida <viniciusgalmeida@gmail.com>
2023-01-11 11:13:18 -0300

    App.js atualization

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 
+const category = 'Posts da semana';
+
 function App() {
   return (
     <>
-      <h1>Componente App</h1>
-      <h2>Subtítulo</h2>
+      <h1>JStack's Blog {console.log('Log de dentro do JSX')}</h1>
+      <h2>{category}</h2>
       
     </>
   );
