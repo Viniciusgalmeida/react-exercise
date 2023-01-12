924559059e484497be2bdf7b7c5cad4969e84124
Viniciusgalmeida <viniciusgalmeida@gmail.com>
2023-01-12 11:37:14 -0300

    Beginning of states class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import "./App.css";
+import Post from "./Post";
+import Header from "./Header";
 
-const category = 'Posts da semana';
+const posts = [
+  { title: "Title#01", subtitle: "Sub#01", likes: 20 },
+  { title: "Title#02", subtitle: "Sub#02", likes: 10 },
+  { title: "Title#03", subtitle: "Sub#03", likes: 50 },
+  { title: "Title#04", subtitle: "Sub#04", likes: 50 },
+];
 
 function App() {
+  function handleRefresh() {
+    console.log('Clicou');
+  }
+
   return (
     <>
-      <h1>JStack's Blog {console.log('Log de dentro do JSX')}</h1>
-      <h2>{category}</h2>
-      
+      <Header>
+        <h2>
+          Posts da semana
+          <button>Atualizar</button>
+        </h2>
+      </Header>
+
+      <hr />
+
+      {posts.map((post) => (
+        <Post
+          key={post.title}
+          likes={post.likes}
+          post={{
+            title: post.title,
+            subtitle: post.subtitle,
+          }}
+        />
+      ))}
     </>
   );
 }
---
+++ src/Post.js
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+export default function Post(props) {
+
+  return (
+    <>
+      <article>
+        <strong>{props.post.title}</strong>
+        <br />
+        <small>{props.post.subtitle}</small>
+        <br />
+        Likes: {props.likes / 2}
+      </article>
+      <br />
+    </>
+  );
+}
+
+Post.propTypes = {
+  likes: PropTypes.number.isRequired,
+  post: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.node,
+  }).isRequired,
+};
