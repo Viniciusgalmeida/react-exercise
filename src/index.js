c08fd6824a32f393f7111e6e42e4024bfd0670e0
Viniciusgalmeida <viniciusgalmeida@gmail.com>
2023-01-26 19:38:32 -0300

    Conteúdo até a aula Trabalhando com styled components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import GlobalStyle from "./styles/global";
+
+import App from "./components/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <>
+    <GlobalStyle />
     <App />
-    
-  </React.StrictMode>
+  </>
 );
-
